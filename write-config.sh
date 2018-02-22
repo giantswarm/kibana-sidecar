687b68903912c5fc32a8d79e40d8d3d4811d42ee
Marian Steinbach <marian@giantswarm.io>
2018-02-22 11:46:22 +0100

    Make escaping more robust

--- write-config.sh
+++ write-config.sh
@@ -8,7 +8,7 @@
 INDEX=.kibana
 
 # Check env variable
-if [ "$ELASTICSEARCH_ENDPOINT" == "" ]; then
+if [ "${ELASTICSEARCH_ENDPOINT}" == "" ]; then
   echo "ERROR: Environment variable ELASTICSEARCH_ENDPOINT is not set. Exiting."
   exit 1
 fi
@@ -18,24 +18,24 @@ do
 
   # Wait for Elasticsearch to become available
   OKAY="unset"
-  curl -s --head "$ELASTICSEARCH_ENDPOINT"|grep -q "HTTP/1.1 200"
+  curl -s --head "${ELASTICSEARCH_ENDPOINT}"|grep -q "HTTP/1.1 200"
   OKAY=$?
   while [ "$OKAY" != "0" ]; do
     echo "Waiting for Elasticsearch to become available"
     sleep 10
-    curl -s --head "$ELASTICSEARCH_ENDPOINT"|grep -q "HTTP/1.1 200"
+    curl -s --head "${ELASTICSEARCH_ENDPOINT}"|grep -q "HTTP/1.1 200"
     OKAY=$?
   done
 
 
   # Check if index exists
-  curl -s --head "$ELASTICSEARCH_ENDPOINT/$INDEX"|grep -q "HTTP/1.1 200"
+  curl -s --head "${ELASTICSEARCH_ENDPOINT}/${INDEX}"|grep -q "HTTP/1.1 200"
   OKAY=$?
 
   if [ "$OKAY" != "0" ]; then
     # Create Index
     echo "Creating index .kibana"
-    curl -s -XPUT "$ELASTICSEARCH_ENDPOINT/$INDEX" \
+    curl -s -XPUT "${ELASTICSEARCH_ENDPOINT}/${INDEX}" \
       -H 'Content-Type: application/json' \
       -d @config/index-mapping.json
     echo ""
@@ -46,7 +46,7 @@ do
   curl -s -XPUT \
     -H 'Content-Type: application/json' \
     -d @config/indexpattern.json \
-    "$ELASTICSEARCH_ENDPOINT/$INDEX/doc/index-pattern:giantswarm"
+    "${ELASTICSEARCH_ENDPOINT}/${INDEX}/doc/index-pattern:giantswarm"
   echo ""
 
   # Write config document
@@ -54,7 +54,7 @@ do
   curl -s -XPUT \
     -H 'Content-Type: application/json' \
     -d @config/config.json \
-    "$ELASTICSEARCH_ENDPOINT/$INDEX/doc/config:6.1.1"
+    "${ELASTICSEARCH_ENDPOINT}/${INDEX}/doc/config:6.1.1"
   echo ""
 
 	sleep 3600
